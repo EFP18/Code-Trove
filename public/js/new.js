b2eeda5e164d5eeecf35b165e0d05d3a6a23c2b0
Thanh Doan <doan.teddy@gmail.com>
2023-06-01 22:22:22 -0400

    bug fixes for log in and signup

--- public/js/new.js
+++ public/js/new.js
@@ -1,21 +1,26 @@
-const newFormHandler = async function(event) {
+const newFormHandler = async function (event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="title"]').value;
-  const body = document.querySelector('input[name="new-snippet"]').value;
+  const title = document.querySelector('input[name="post-title"]').value;
+  const category = document
+    .querySelector('input[name="post-language"]')
+    .value.trim();
+  const body = document.querySelector('input[name="post-content"]').value;
 
   await fetch(`/api/post`, {
-    method: 'POST', 
+    method: 'POST',
     body: JSON.stringify({
-      title, 
-      body
-    }), 
-    headers: { 'Content-Type': 'application/json'}
+      title,
+      body,
+      category,
+    }),
+    headers: { 'Content-Type': 'application/json' },
   });
 
-  document.location.replace('/profile');  
+  // TODO: add profile id
+  document.location.replace('/profile');
 };
 
 document
-.querySelector('#new-snippet-form')
-.addEventListener('submit', newFormHandler);
+  .querySelector('#new-snippet-form')
+  .addEventListener('submit', newFormHandler);
